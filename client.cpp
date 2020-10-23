b2106d1faa7f35ab275b0f50b8ae972d37182b61
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-10-23 18:57:27 -0400

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,40 +1,58 @@
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
+#include <iostream>
+#include <stdlib.h> 
 #include <unistd.h> 
 #include <string.h> 
-#define PORT 8080 
-   
-int main(int argc, char const *argv[]) 
-{ 
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
-    char *hello = "Hello from client"; 
-    char buffer[1024] = {0}; 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
-   
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
-    { 
-        printf("\nInvalid address/ Address not supported \n"); 
-        return -1; 
-    } 
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection Failed \n"); 
-        return -1; 
-    } 
-    send(sock , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
-    valread = read( sock , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    return 0; 
+#include <time.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#include <vector>
+
+using namespace std;
+
+#define PORT	 1027
+
+
+int main() { 
+	int sockfd, count;
+	clock_t start, end;
+	socklen_t len;
+	char buffer[1024];
+	struct sockaddr_in servaddr, cliaddr; 
+	
+	// Create a UDP socket
+	// Notice the use of SOCK_DGRAM for UDP packets
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	
+	memset(&servaddr, 0, sizeof(servaddr)); 
+	memset(&cliaddr, 0, sizeof(cliaddr)); 
+	
+	// Fill server information 
+	servaddr.sin_family = AF_INET; // IPv4 
+	servaddr.sin_addr.s_addr = INADDR_ANY; // localhost
+	servaddr.sin_port = htons(PORT); // port number
+	
+	if(setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(tv)) < 0) {
+   		 perror("Error");
+	}
+	//Keep track of number of pings
+        count = 0;
+	//send 10 pings in a loop
+
+	len = sizeof(servaddr);
+	while(count<10){
+	for(int i = 0; i<10; i++){
+		sendto(sockfd, (const char *) buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *) &servaddr, len);
+		int n;
+	        n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
+		//buffer[n] = '\0';
+		valread = read(sockfd, buffer, 1024);
+		
+		  cout << "Ping Number " << count << "Round Trip Time " << end << " count" << endl;
+		count++;
+	}
+	
+	}
+	return 0; 
 } 
