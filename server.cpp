254540d2c87a95c25b80ca55d36fea005c75cd01
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-10-23 19:04:32 -0400

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -46,7 +46,7 @@ int main() {
 			MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
 		buffer[n] = '\0';
 
-		sendto(sockfd, hello, strlen(hello), 
+		sendto(sockfd, buffer, strlen(buffer), 
 			MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
 	}
 	return 0; 
