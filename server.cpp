52e23515dec411877227e457f79ca69b3565e6ef
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-10-23 18:57:51 -0400

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,64 +1,57 @@
-// Server side C/C++ program to demonstrate Socket programming 
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
-#define PORT 8080 
-int main(int argc, char const *argv[]) 
-{ 
-    int server_fd, new_socket, valread; 
-    struct sockaddr_in address; 
-    int opt = 1; 
-    int addrlen = sizeof(address); 
-    char buffer[1024] = {0}; 
-    char *hello = "Hello from server"; 
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-       
-    // Forcefully attaching socket to the port 8080 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
-    { 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
-    } 
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
-         int count = 0;
-    while(count<2) { 
-    // Forcefully attaching socket to the port 8080 
-    if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
+// UDP Pinger
 
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-         
-    } 
-    valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
-        count++;
-    }
-    return 0; 
+// Must have this server running before you can run the UDP Pinger Client code
+
+#include <iostream>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <time.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+#define PORT	 1027
+
+
+int main() { 
+	int sockfd, n;
+	socklen_t len;
+	char buffer[1024];
+	char *hello = "Hello from server";
+	struct sockaddr_in servaddr, cliaddr; 
+	
+	// Create a UDP socket
+	// Notice the use of SOCK_DGRAM for UDP packets
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	
+	memset(&servaddr, 0, sizeof(servaddr)); 
+	memset(&cliaddr, 0, sizeof(cliaddr)); 
+	
+	// Fill server information 
+	servaddr.sin_family = AF_INET; // IPv4 
+	servaddr.sin_addr.s_addr = INADDR_ANY; // localhost
+	servaddr.sin_port = htons(PORT); // port number
+	
+	// Bind the socket with the server address 
+	bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+	
+	// random generator
+	srand(time(0));
+
+        while (1) {
+		//Receive the client packet along with the address it is coming from
+		n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), 
+			MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+		//buffer[n] = '\0';
+
+		//If a random number in the range of 0 to 10 is less than 4,
+		//we consider the packet lost and do not respond
+
+		//Otherwise, the server responds
+		sendto(sockfd, hello, strlen(hello), 
+			MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
+	}
+	return 0; 
 } 
