96f1472918ecde20e79b6fb5f2707c8e7614d903
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-10-23 18:19:26 -0400

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -49,7 +49,7 @@ int main(int argc, char const *argv[])
                        (socklen_t*)&addrlen))<0) 
     { 
         perror("accept"); 
-        exit(EXIT_FAILURE); 
+         
     } 
     valread = read( new_socket , buffer, 1024); 
     printf("%s\n",buffer ); 
