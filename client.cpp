8f32569f23139a12ed6fd43b029230fe7e306049
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-10-23 19:53:03 -0400

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -13,13 +13,14 @@ using namespace std;
 
 #define PORT	 1027
 
-
-int main() { 
-	int sockfd, count;
-	clock_t start, end;
+//using command line args to control which client
+int main(int args, char *argv[])) { 
+	int sockfd;
 	socklen_t len;
 	char buffer[1024];
+	char * message = argv[1];
 	struct sockaddr_in servaddr, cliaddr; 
+	string msg;
 	
 	// Create a UDP socket
 	// Notice the use of SOCK_DGRAM for UDP packets
@@ -32,21 +33,7 @@ int main() {
 	servaddr.sin_family = AF_INET; // IPv4 
 	servaddr.sin_addr.s_addr = INADDR_ANY; // localhost
 	servaddr.sin_port = htons(PORT); // port number
-	
-	//Set timeout for 1 second
-	struct timeval tv;
-	tv.tv_sec = 1;
-	tv.tv_usec = 0;
-	if(setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(tv)) < 0) {
-   		 perror("Error");
-	}
-	//Keep track of number of pings
-        count = 0;
-	
-	//send 10 pings in a loop
-	for(int i = 0; i<10; i++){
-		//Get current time
-	        start = time(0);
+  
 		len = sizeof(servaddr);
 		//Send message to server
 		sendto(sockfd, (const char *) buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *) &servaddr, len);
@@ -54,17 +41,9 @@ int main() {
 		int n;
 	        n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
 		buffer[n] = '\0';
-		//See how long has passed
-		double total = (double)difftime(time(0), start);
-		//Check if ping sent for longer than a second
-		if(total >= 1.0){
-		  cout << "Connection Time out on Ping Number " << count << endl;
-		}
-		else{
-		  cout << "Ping Number " << count << "Round Trip Time " << end << " Seconds" << endl;
-		}
-		count++;
-	
-	}
+	//get server message and print out
+		msg = string(buffer);
+		cout << message << "\n" << msg;
+		
 	return 0; 
 } 
